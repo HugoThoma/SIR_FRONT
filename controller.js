3b35f2ec244c9ee5aae03c1530193d1cc797ff15
Hugo <hugo.thomas22@hotmail.Fr>
2023-03-30 11:22:04 +0200

    Avancée jusqu'à la question 12 (non réalisée)

--- controller.js
+++ controller.js
@@ -9,9 +9,45 @@ function Pencil(ctx, drawing, canvas) {
 
 	// Liez ici les widgets à la classe pour modifier les attributs présents ci-dessus.
 
+	document.getElementById('butRect').onclick = (_) => this.currEditingMode = editingMode.rect;
+	document.getElementById('butLine').onclick = (_) => this.currEditingMode = editingMode.line;
+	document.getElementById('spinnerWidth').onchange = (e) => this.currLineWidth = e.target.value;
+	document.getElementById('colour').onchange = (e) => this.currColour = e.target.value;
+
 	new DnD(canvas, this);
 
 	// Implémentez ici les 3 fonctions onInteractionStart, onInteractionUpdate et onInteractionEnd
+
+	this.onInteractionStart = function (dnd){
+
+		this.currentShape = new Rectangle();
+
+	}.bind(this);
+
+	this.onInteractionUpdate = function (dnd){
+		//console.log('update');
+		if(this.currEditingMode === editingMode.rect){
+			this.currentShape = new Rectangle(dnd.initX, dnd.initY, this.currLineWidth, this.currColour, dnd.finalY - dnd.initY, dnd.finalX - dnd.initX);
+
+		}else{
+			this.currentShape = new Line(dnd.initX, dnd.initY, this.currLineWidth, this.currColour, dnd.finalX, dnd.finalY);
+		}
+
+		drawing.paint(ctx, canvas);
+		this.currentShape.paint(ctx);
+	}.bind(this);
+
+	this.onInteractionEnd = function (dnd){
+		drawing.shapeArray.push(this.currentShape);
+		drawing.paint(ctx, canvas);
+		this.currentShape.paint(ctx);
+	}.bind(this);
 };
 
+function remove(drawing, index, ctx, canvas){
+	console.log(index);
+	drawing.shapeArray.delete(index);
+	document.getElementById('liRemove' + index).remove();
+	drawing.paint(ctx, canvas);
+}
 
--- interaction.js
+++ interaction.js
@@ -3,8 +3,55 @@
 // L'interacteur viendra dans un second temps donc ne vous en souciez pas au départ.
 function DnD(canvas, interactor) {
 	// Définir ici les attributs de la 'classe'
+// -- Q1 --
+    this.initX = 0;
+    this.initY = 0;
+    this.finalX = 0;
+    this.finalY = 0;
+    this.isclicked = false;
+    this.interactor = interactor;
 
-	// Developper les 3 fonctions gérant les événements
+  // Developper les 3 fonctions gérant les événements
+// -- Q2 --
+    this.clicsouris = function (evt){
+
+        var pos = getMousePosition(canvas, evt);
+        this.initX = pos.x;
+        this.initY = pos.y;
+        this.isclicked = true;
+        //console.log(pos);
+        this.interactor.onInteractionStart(this);
+
+    }.bind(this);
+
+    this.deplacementsouris = function (evt){
+        if(this.isclicked){
+            var pos = getMousePosition(canvas, evt);
+            this.finalX = pos.x;
+            this.finalY = pos.y;
+            this.isclicked = true;
+            //console.log(pos);
+            this.interactor.onInteractionUpdate(this);
+
+
+        }
+
+    }.bind(this)
+
+    this.relachersouris = function (evt){
+        var pos = getMousePosition(canvas, evt);
+        this.finalX = pos.x;
+        this.finalY = pos.y;
+        this.isclicked = false;
+        //console.log(pos);
+        this.interactor.onInteractionEnd(this);
+
+
+    }.bind(this)
+// -- Q3 --
+    canvas.addEventListener('mousedown', this.clicsouris,false)
+    canvas.addEventListener('mousemove', this.deplacementsouris, false)
+    canvas.addEventListener('mouseup', this.relachersouris, false)
 
 	// Associer les fonctions précédentes aux évènements du canvas.
 };
